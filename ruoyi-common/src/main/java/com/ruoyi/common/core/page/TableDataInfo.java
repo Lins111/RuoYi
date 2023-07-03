d2cb50e35b72e6f9fa29a677df7757b53ed42adc
Zhang Yixuan <132724214+Lins111@users.noreply.github.com>
2023-07-03 10:08:51 +0800

    Update TableDataInfo.java
        /** 总记录数 */

--- ruoyi-common/src/main/java/com/ruoyi/common/core/page/TableDataInfo.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/page/TableDataInfo.java
@@ -46,6 +46,17 @@ public class TableDataInfo implements Serializable
     public long getTotal()
     {
         return total;
+            /** 总记录数 */
+    private long total;
+
+    /** 列表数据 */
+    private List<?> rows;
+
+    /** 消息状态码 */
+    private int code;
+
+    /** 消息内容 */
+    private String msg;
     }
 
     public void setTotal(long total)
@@ -66,6 +77,17 @@ public class TableDataInfo implements Serializable
     public int getCode()
     {
         return code;
+            /** 总记录数 */
+    private long total;
+
+    /** 列表数据 */
+    private List<?> rows;
+
+    /** 消息状态码 */
+    private int code;
+
+    /** 消息内容 */
+    private String msg;
     }
 
     public void setCode(int code)
